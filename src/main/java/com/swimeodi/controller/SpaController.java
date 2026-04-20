967919ec1fc5b07b0026d6dbfaf490416a27c698
mgseong <mgseongdev@gmail.com>
2026-04-20 16:14:38 +0900

    initial commit

---
+++ src/main/java/com/swimeodi/controller/SpaController.java
@@ -0,0 +1,13 @@
+package com.swimeodi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SpaController {
+
+    @GetMapping(value = { "/", "/{path:[^\\.]*}", "/{path:[^\\.]*}/**" })
+    public String spa() {
+        return "forward:/index.html";
+    }
+}
