f6fb5bab7bb1d194c3a55c893d006634f0961855
mgseong <s.min.g7904@gmail.com>
2026-04-21 17:13:31 +0900

    Fix: serve index.html directly via ClassPathResource

--- src/main/java/com/swimeodi/controller/SpaController.java
+++ src/main/java/com/swimeodi/controller/SpaController.java
@@ -1,13 +1,21 @@
 package com.swimeodi.controller;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
 
 @Controller
 public class SpaController {
 
     @GetMapping(value = { "/", "/{path:[^\\.]*}", "/{path:[^\\.]*}/**" })
-    public String spa() {
-        return "forward:/index.html";
+    public ResponseEntity<byte[]> spa() throws IOException {
+        ClassPathResource resource = new ClassPathResource("static/index.html");
+        return ResponseEntity.ok()
+                .contentType(MediaType.TEXT_HTML)
+                .body(resource.getContentAsByteArray());
     }
 }
